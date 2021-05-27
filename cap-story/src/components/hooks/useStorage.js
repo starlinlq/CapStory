0a4e98e3ac7fc5ebfb21681ac52e880974f8c521
starlinlq <67881697+starlinlq@users.noreply.github.com>
2021-05-27 00:04:05 -0400

    new package

--- cap-story/src/components/hooks/useStorage.js
+++ cap-story/src/components/hooks/useStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectStorage } from "../../fireStore/config";
+import { projectStorage } from "../../firestore/config";
 
 const useStorage = (file) => {
   const [progress, setProgress] = useState(0);
